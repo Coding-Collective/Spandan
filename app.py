4f93284c66dc5f709802ae506d813125110ac922
Delicia Fernandes <fernandesdelicia07@gmail.com>
2022-04-07 10:22:45 +0530

    Dels: Working connection made

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import pandas as pd
+import requests
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -10,42 +12,52 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
+
+
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
 
 @app.route('/blog')
 def blog():
     return render_template("blog.html")
 
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
 
-@app.route('/model')
+
+@app.route('/model', methods=['GET', 'POST'])
 def model():
-	data = pd.read_csv("data/final-data.csv")
+    data = pd.read_csv("data/final-data.csv")
+	
+    # For rendering results on HTML GUI
+    marathi_sentence = request.form.get("emotion_calculator")
+    print(marathi_sentence)
 
 	# inputstr=['मी काम करतोय']
-	# मी काम करतोय
-	marathi_sentence = input()
-	inputstr=[marathi_sentence]
+	# marathi_sentence = input()
+    inputstr=[marathi_sentence]
 
-	if (inputstr in data['comment'].values):
-		print()
-
-	rslt_df = data[data['comment'].isin(inputstr)]
+    if (inputstr in data['comment'].values):
+        print()
+    
+    rslt_df = data[data['comment'].isin(inputstr)]
+    print(rslt_df)
 	# rslt_df['comment']
 	# rslt_df['rating']
 	# rslt_df['comment marathi']
 	# rslt_df['stop word removed comment']
 	
-	return render_template("model.html")
+    return render_template("model.html", sentiment=rslt_df['rating'])
 
 # main driver function
 if __name__ == '__main__':
